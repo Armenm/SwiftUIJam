a92c01e7638aa225337211575fcf07628b26cb8b
armenm <armen.mkrtchian@gmail.com>
2021-02-20 10:59:36 -0500

    Initial Commit

---
+++ SwiftUIJam/SwiftUIJamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIJamApp.swift
+//  SwiftUIJam
+//
+//  Created by Armen Mkrtchyan on 2021-02-20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIJamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
